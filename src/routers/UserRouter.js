424616548e6a51e43da1053ee03e61d96fa58503
tloc00 <nguyen.ntl.sun04132000@gmail.com>
2020-12-21 17:56:50 +0700

    update profile page

--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -35,26 +35,25 @@ router.get("/", async (req, res) => {
     res.render("index")
 });
 
-// router.get("/login", async (req, res) => {
-//   if (req.isAuthenticated()) {
-//     return res.redirect("/");
-//   }
-//   res.render("login");
-// });
-
-// router.post(
-//   "/login",
-//   passport.authenticate("local", {
-//     successRedirect: "/",
-//     failureRedirect: "/login",
-//     failureFlash: true,
-//   })
-// );
-
-
 router.get("/login", async (req, res) => {
-    console.log(req.user);
+    if (req.isAuthenticated()) {
+        return res.redirect("/");
+    }
     res.render("login");
+});
+
+router.post(
+    "/login",
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/login",
+        failureFlash: true,
+    })
+);
+
+router.get("/profile", async (req, res) => {
+    console.log(req.user);
+    res.render("profile");
 });
 
 router.get("/register", async (req, res) => {
@@ -97,9 +96,9 @@ router.get("/logout", async (req, res) => {
 });
 
 
-router.get('*',(req,res)=>{
-    res.render('error',{
-        error:'The page you are trying to connect does not exist or you are not authorized to access!',
+router.get('*', (req, res) => {
+    res.render('error', {
+        error: 'The page you are trying to connect does not exist or you are not authorized to access!',
     })
 })
 
